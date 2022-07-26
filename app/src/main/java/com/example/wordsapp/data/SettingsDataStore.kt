8710f67d420ad75e312789bd74130f54e102b589
Gabriel <gabriel99luques@gmail.com>
2022-07-26 17:47:08 -0300

    Write to the Preferences DataStore

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.recyclerview.widget.LinearLayoutManager
 
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
@@ -16,4 +18,10 @@ val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
 class SettingsDataStore(context: Context) {
     private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layour_manager")
 
+}
+
+suspend fun saveLayoutToPreferencesStore(isLinearLayoutManager: Boolean, context: Context) {
+    context.dataStore.edit { preferences ->
+        preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+    }
 }
